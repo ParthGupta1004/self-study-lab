6327110c866f5a51506954f0099805b25a356078
ParthGupta1004 <parthgupta5797@gmail.com>
2026-08-28 00:16:05 +0530

    feat: resuable modal component

--- product-store/src/App.jsx
+++ product-store/src/App.jsx
@@ -41,22 +41,33 @@ function App() {
       </div>
 
 
-      <button
-        onClick={() => setIsOpen(true)}
-        className="mt-8 rounded-lg bg-teal-700 px-5 py-3 text-white hover:bg-teal-800"
-      >
-        Open Modal
-      </button>
+      {/* 3.3 Transform Trap Test  and we used it by keeping the box model outside the test card*/}
+      <div className="mt-10 w-80 rounded-xl border border-slate-300 p-6 transition-transform hover:scale-105">
 
+        <h2 className="font-display text-xl font-bold">
+          Test Card
+        </h2>
+
+        <p className="mt-2 text-slate-600">
+          This card has a transform.
+        </p>
+
+        <button
+          onClick={() => setIsOpen(true)}
+          className="mt-5 rounded-lg bg-teal-700 px-4 py-2 text-white hover:bg-teal-800"
+        >
+          Open Modal
+        </button>
+      </div>
 
       <Modal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        title="Test Modal"
-      >
-        <p className="leading-relaxed">
-          This is my reusable modal component.
-        </p>
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          title="Test Modal"
+        >
+          <p className="leading-relaxed">
+            This modal is inside the transformed card.
+          </p>
       </Modal>
 
     </div>
